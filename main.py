0b1f9f457f16969dda8fc533ae5e7ab98cb1843f
RPomer <robert.pomeroy@gmail.com>
2017-02-18 12:18:33 -0700

    Update

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import mystuff
 
-print "1"
 thing = mystuff.MyStuff()
-print "2"
 thing.apple()
-print "3"
 print thing.tangerine
--- mystuff.py
+++ mystuff.py
@@ -4,5 +4,6 @@ class MyStuff(object):
     def __init__(self):
         self.tangerine = "And now a thousand years between"
 
-    def apple(self):
+    @staticmethod
+    def apple():
         print "I AM CLASSY APPLES!"
